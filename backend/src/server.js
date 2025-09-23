78b6c41eba8cc889006c5c3fac5539064b10959d
Zara khan <zarakhang067@gmail.com>
2025-09-23 01:27:26 -0700

    some chnages for deploymnet 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+
 import authRouter from "./routers/auth.route.js";
 import messageRouter from "./routers/message.route.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRouter);
@@ -22,4 +26,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
