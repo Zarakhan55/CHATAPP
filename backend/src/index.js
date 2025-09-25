dc63edf17cf346641cc7c3224fc2c21c8c8528c4
Zara khan <zarakhang067@gmail.com>
2025-09-25 10:23:40 -0700

    complte signup login design

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,42 @@
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-const PORT = ENV.PORT || 3000;
-
-app.use(express.json({ limit: "5mb" }));
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("Server running on port: " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
